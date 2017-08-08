fb9606b3636aae02fb88e6673ffd89b830df5f43
liucheng <glucee@hotmail.com>
2017-08-08 18:54:59 +0100

    give an example of info.py

---
+++ info.py
@@ -0,0 +1,33 @@
+##################################################################
+#                                                                #
+# Update the following variables with your own personal details: #
+#                                                                #
+##################################################################
+
+
+# Driving license number (Example: MORGA657054SM9IJ)
+licenceNumber = '******'
+
+# Application reference number 
+# (This number was given when you booked the test. It can be found on your confirmation email.)
+theoryNumber = '*****'
+
+# Email sending details
+
+# The email addresses you wish to send notifications to
+emailAddresses = ['example@example.com', 'MySecondEmailAddress@example.com']
+
+emailSubject = "DSA Cancellations"
+emailFrom = "no-reply@example.com"
+
+# Enter your gmail account details here so that the script can send emails
+emailUsername = 'example@gmail.com'
+emailPassword = 'password'  # the password to your "example@gmail.com" account
+
+# Change this (at your own risk) if you don't use gmail (e.g. to hotmail/yahoo/etc smtp servers
+emailSMTPserver = 'smtp.gmail.com'
+
+# Put in your current test date in the format "Thursday 4 July 2013 2:00pm"; you will be alerted if an earlier slot appears
+
+myTestDateString = 'Monday 23 October 2017 12:33pm'
+
